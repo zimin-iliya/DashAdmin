215102696dadaef294a80ed5f4bbcfafa8111d75
zimin-iliya <zimin.box@gmail.com>
2023-06-06 01:31:06 +0300

    create user connected to mongoDB

--- src/scenes/registration/index.jsx
+++ src/scenes/registration/index.jsx
@@ -1,115 +1,88 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Box, TextField } from "@mui/material";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import axios from "axios";
+import { UserContext } from "../../authorization/UserContext";
 
 const Registration = () => {
+  // const {setUsername,setId} = useContext(UserContext); 
   const [values, setValues] = React.useState({
-    firstName: "",
-    lastName: "",
+    username: "",
     email: "",
     password: "",
   });
 
-  const initialValues = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-  };
 
-  const userSchema = Yup.object().shape({
-    firstName: Yup.string().required("First Name is Required"),
-    lastName: Yup.string().required("Last Name is Required"),
-    email: Yup.string().email("Invalid Email").required("Email is Required"),
-    password: Yup.string().required("Password is Required"),
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
   });
+  async function handleRegister(event) {
+    console.log(values);
+    event.preventDefault();
+    const responce = await axios.post("/register", values);
+    console.log(responce);
+  }
 
-  function handleChange(event) {
+  function handleChange2(event) {
+    event.preventDefault();
     const { name, value } = event.target;
-    setValues({
-      ...values,
-      [name]: value,
+    setValues((prevValues) => {
+      return {
+        ...prevValues,
+        [name]: value,
+      };
     });
   }
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    console.log(values);
-  }
-
   return (
     <div>
-      <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={userSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleFormSubmit,
-        }) => (
-          <form onSubmit={handleFormSubmit}>
-            <TextField
-              fullWidth
-              variant="filled"
-              type="text"
-              label="First Name"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              value={values.firstName}
-              name="firstName"
-              error={!!touched.firstName && !!errors.firstName} //give example of what this is doing
-              helperText={touched.firstName && errors.firstName}
-              sx={{ gridColumn: "span 2" }}
-            />
-            <TextField
-              fullWidth
-              variant="filled"
-              type="text"
-              label="Last Name"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              value={values.lastName}
-              name="lastName"
-              error={!!touched.lastName && !!errors.lastName}
-              helperText={touched.lastName && errors.lastName}
-              sx={{ gridColumn: "span 2" }}
-            />
-            <TextField
-              fullWidth
-              variant="filled"
-              type="text"
-              label="Email"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              value={values.email}
-              name="email"
-              error={!!touched.email && !!errors.email}
-              helperText={touched.email && errors.email}
-              sx={{ gridColumn: "span 2" }}
-            />
-            <TextField
-              fullWidth
-              variant="filled"
-              type="password"
-              label="Password"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              value={values.password}
-              name="password"
-              error={!!touched.password && !!errors.password}
-              helperText={touched.password && errors.password}
-              sx={{ gridColumn: "span 2" }}
-            />
-            <Button color="secondary">Submit</Button>
-          </form>
-        )}
-      </Formik>
+      <form onSubmit={handleRegister}>
+        <TextField
+          fullWidth
+          variant="filled"
+          type="text"
+          label="username"
+          onChange={handleChange2}
+          value={values.username}
+          name="username"
+          sx={{ gridColumn: "span 2" }}
+        />
+        <TextField
+          fullWidth
+          variant="filled"
+          type="text"
+          label="Email"
+          onChange={handleChange2}
+          value={values.email}
+          name="email"
+          sx={{ gridColumn: "span 2" }}
+        />
+        <TextField
+          fullWidth
+          variant="filled"
+          type="password"
+          label="Password"
+          onChange={handleChange2}
+          value={values.password}
+          name="password"
+          sx={{ gridColumn: "span 2" }}
+        />
+        <Button
+          variant="contained"
+          onClick={handleRegister}
+          color="secondary"
+        >
+          Submit
+        </Button>
+      </form>
     </div>
   );
 };
